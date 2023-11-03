eba901a2fcc6b21ff64f24665f8dd48fa708583c
신준혁 <oridori2705@naver.com>
2023-11-03 15:01:53 +0900

    드래그 앤 드롭 기능 간단 구현 및 낙관적 업데이트 추가
    하지만 API 응답이 느린 상태에서 낙관적업데이트를 이용하면서 생기는 데이터 지연 현상

--- src/App.js
+++ src/App.js
@@ -27,12 +27,19 @@ export default function App({ $target }) {
       title: "완료되지않은 일",
       todos: [],
     },
+    onDrop: async (todoId) => {
+      handlerOnDrop(todoId, false);
+    },
   });
+
   const completedTodoList = new TodoList({
     $target,
     initialState: {
       title: "완료된 일",
       todos: [],
+    },
+    onDrop: async (todoId) => {
+      handlerOnDrop(todoId, true);
     },
   });
 
@@ -45,4 +52,36 @@ export default function App({ $target }) {
     });
   };
   fetchTodos();
+
+  //낙관적 업데이트
+  const handlerOnDrop = async (todoId, toggleData) => {
+    const nextTodos = [...this.state.todos];
+    console.log(nextTodos, todoId);
+    const todoIndex = nextTodos.findIndex((todo) => todo._id === todoId);
+    nextTodos[todoIndex].isCompleted = toggleData;
+    this.setState({
+      ...this.state,
+      todos: nextTodos,
+    });
+    await request(`/${todoId}/toggle`, {
+      method: "PUT",
+    });
+    await fetchTodos();
+  };
 }
+
+//만약 onDrop이 호출될 때마다 API호출로 데이터를 바꾸고
+//바뀐 데이터를 다시 API호출로 받아오게 된다면
+//서버가 느릴 때 대처하기가 힘들게 된다.
+
+//이를 해결 하는 방법은 2가지가 있다.
+// 1. 낙관적 업데이트
+// 2. 로딩화면 구현
+
+//만약 낙관적 업데이트 방법을 이용했다면
+//API호출이 느린 상태에서 빠른속도로 드래그를 하다보면
+// 현재 위 코드에서는 드래그된 item들이 낙관적 업데이트 후 fetchTodos로 인해
+// 데이터가 미묘한 차이로 이질적인 움직임을 보여준다.
+
+//해결 방법
+// 큐나 스택을 이용
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-export default function TodoList({ $target, initialState }) {
+export default function TodoList({ $target, initialState, onDrop }) {
   const $todoList = document.createElement("div");
 
   $todoList.setAttribute("draggable", "true");
@@ -17,21 +17,48 @@ export default function TodoList({ $target, initialState }) {
             <h2>${title}</h2>
             <ul>
                 ${todos
-                  .map((todo) => `<li draggable="true" >${todo.content}</li>`)
+                  .map(
+                    (todo) =>
+                      `<li data-id="${todo._id}" draggable="true" >${todo.content}</li>`
+                  )
                   .join("")}
             </ul>
             ${todos.length === 0 ? "설정된 일이 없습니다." : ""}
         `;
   };
   this.render();
-
+  //datatransfer : 드래그할 때 여러가지 값들을 넣을 수 있음
   //datatransfer라는 객체를 통해서 드랍할 곳으로 데이터를 어떤 걸 보낼지 정의
   //드랍 받은 곳은 드래그 시작했을 때 datatransfer 데이터를 꺼내와서 어떤게 드랍됐는지를 판단해서 사용
 
   //drag : 드래그하면서 효과를 줄 수  있음
   //dragstart : 드래그 시작할 때
   //dragend: 드래그가 끝날 때
-  $todoList.addEventListener("drag", (e) => {
-    e.dataTransfer.dropEffect = "copy"; //copy, move, link 3가지 타입이 있음
+
+  //datatransfer을 이용해서 어떤 요소가 드래그가 시작이 됐는지 그 값을 넣고,
+  // 드랍하는 곳에서 그 값을 꺼내서 쓴다.
+  // <>마다 있는 data-id를 이용해 작업한다.
+  $todoList.addEventListener("dragstart", (e) => {
+    const $li = e.target.closest("li");
+
+    e.dataTransfer.setData("todoId", $li.dataset.id);
+  });
+
+  //dragover : MDN에서 드래그 사용할 때 사용하는 이벤트
+  //drop : e.preventDefault()를  꼭 해줘야한다고 MDN에 나옴
+  $todoList.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move"; //copy : 복사 , move: 복사 x 이동만, link 3가지 타입이 있음
+  });
+
+  $todoList.addEventListener("drop", (e) => {
+    e.preventDefault();
+    const droppedTodoId = e.dataTransfer.getData("todoId");
+
+    // 현재 TodoList의 Todo가 아닌 경우 상위 컴포넌트에 알림
+    const { todos } = this.state;
+    if (!todos.find((todo) => todo._id === droppedTodoId)) {
+      onDrop(droppedTodoId);
+    }
   });
 }
